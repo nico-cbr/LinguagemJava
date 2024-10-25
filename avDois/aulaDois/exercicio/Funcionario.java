dd0dbc6a2daab9b4db388ff7391df2c8f9fa7b6d
Nicoly <carvalhonicoly@hotmail.com>
2024-10-25 09:37:38 -0300

    exercicio

---
+++ avDois/aulaDois/exercicio/Funcionario.java
@@ -0,0 +1,33 @@
+public class Funcionario {
+  private String nome;
+  private int idade;
+  private int salario;
+
+  // getters
+  public String getNome() {
+    return nome;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public int getSalario() {
+    return salario;
+  }
+
+  // setters
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+
+  public void setSalario(int salario) {
+    this.salario = salario;
+  }
+
+}
---
+++ avDois/aulaDois/exercicio/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite o nome do funcionario: ");
+    String nome = scanner.nextLine();
+
+    System.out.print("Digite a idade do funcionario: ");
+    int idade = scanner.nextInt();
+
+    System.out.print("Digite o salário do funcionario: ");
+    int salario = scanner.nextInt();
+
+    Funcionario funcionario = new Funcionario();
+    funcionario.setNome(nome);
+    funcionario.setIdade(idade);
+    funcionario.setSalario(salario);
+
+    System.out.println("Nome: " + funcionario.getNome());
+    System.out.println("Idade: " + funcionario.getIdade());
+    System.out.println("Salario: " + funcionario.getSalario());
+
+    scanner.close();
+  }
+}
